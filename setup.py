4451b23e02abc5bb1b911e6a35ec4f24a9158787
ava-wenjie <wenjie.he@avatronics.com>
2023-12-28 12:33:49 +0100

    Add license file and project configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="my_py_fns",
+    version="0.0.1",
+    description="My private package from private github repo",
+    url="git@github.com:Jaitoh/my_py_fns.git",
+    author="Wenjie He",
+    author_email="heyvinci@outlook.com",
+    license="unlicense",
+    packages=["my_py_fns"],
+    zip_safe=False,
+)
